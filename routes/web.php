1d66c1cb1c3998777029ec2d6b65e516c7b68785
bhanudv <chandrabhanudv@gmail.com>
2022-01-03 22:02:22 +0530

    first commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BrandController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function() {
+    return view('welcome');
+});
+
+Route::get('adv', function() {
+    return view('advanced');
+});
+
+Route::get('/brands', [BrandController::class, 'index'])->name('brands.index');
+Route::get('/brands/create', [BrandController::class, 'create'])->name('brands.create');
+Route::post('/brands', [BrandController::class, 'store'])->name('brands.store');
+Route::get('brands/{id}', [BrandController::class, 'show'])->name('brands.show');
+Route::delete('brands/destory', [BrandController::class, 'destory'])->name('brands.destorys');
